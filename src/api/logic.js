181bd61075c439d329b6bd7ace9a7ea13176cf04
PabloMad75 <pablo.madrid.riquelme@gmail.com>
2023-07-11 22:41:29 -0400

    cambios js prueba base graphic

--- src/api/logic.js
+++ src/api/logic.js
@@ -1,3 +1,4 @@
+const urlIndicadorApi = 'https://mindicador.cl/api/';
 const btnConsult = document.getElementById("btnconsult");
 btnConsult.addEventListener("click", function (event) {
   event.preventDefault(); // Evita el comportamiento predeterminado del envío del formulario
@@ -11,40 +12,8 @@ btnConsult.addEventListener("click", function (event) {
   // Realiza las operaciones que necesites con el valor seleccionado
 });
 
-// axios.get('https://mindicador.cl/api')
-//   .then(response => {
-//     const data = response.data;
-//     const dolarValue = data.dolar.valor;
-//     const ctx = document.getElementById('myChart').getContext('2d');
-//     new Chart(ctx, {
-//       type: 'bar',
-//       data: {
-//         labels: ['Dólar'],
-//         datasets: [{
-//           label: 'Valor',
-//           data: [dolarValue],
-//           backgroundColor: 'rgba(75, 192, 192, 0.2)',
-//           borderColor: 'rgba(75, 192, 192, 1)',
-//           borderWidth: 1
-//         }]
-//       },
-//       options: {
-//         scales: {
-//           y: {
-//             beginAtZero: true
-//           }
-//         }
-//       }
-//     });
-//   })
-//   .catch(error => {
-//     // console.error('Error al obtener los datos de la API:', error);
-//   });
-
-
-
 axios
-  .get("https://mindicador.cl/api/")
+  .get(urlIndicadorApi)
   .then((response) => {
     // El código dentro de esta función se ejecutará cuando se reciba una respuesta exitosa de la API
     console.log(response.data);
